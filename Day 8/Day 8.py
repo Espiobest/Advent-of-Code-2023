f35f855d6c967d0dee5098e5fd7935b511749c04
Espiobest <59823894+Espiobest@users.noreply.github.com>
2023-12-09 05:09:30 -0500

    Add day 8,9 sols

---
+++ Day 8/Day 8.py
@@ -0,0 +1,45 @@
+import itertools
+import math
+
+with open('data.txt', 'r') as r:
+    lines = r.read().splitlines()
+
+instructions = itertools.cycle(lines[0])
+mapping = {}
+for line in lines[2:]:
+    origin, path = line.split(" = ")
+    mapping[origin] = [item.strip() for item in path[1:-1].split(',')]
+
+
+steps = 0
+dest = "AAA"
+while dest != "ZZZ":
+    steps += 1
+    cur = next(instructions)
+
+    if cur == "L":
+        dest = mapping[dest][0]
+    else:
+        dest = mapping[dest][1]
+
+
+print("Part 1:", steps)
+
+dest = [i for i in mapping if i.endswith("A")]
+all_steps = []
+
+for i in dest:
+    steps = 0
+    dest = i
+    while not dest.endswith("Z"):
+        steps += 1
+        cur = next(instructions)
+
+        if cur == "L":
+            dest = mapping[dest][0]
+        else:
+            dest = mapping[dest][1]
+
+    all_steps.append(steps)
+
+print("Part 2:", math.lcm(*all_steps))
---
+++ Day 9/Day 9.py
@@ -0,0 +1,26 @@
+with open('data.txt', 'r') as r:
+    lines = r.read().splitlines()
+
+tot = 0
+tot2 = 0
+for line in lines:
+    values = [*map(int, line.split())]
+    history = []
+
+    while any(values):
+        arr = [j - i for i,j in zip(values, values[1:])]
+        history.append(values)
+        values = arr
+    history.append(arr)
+
+    prev = 0
+    prev2 = 0
+    for i in history[::-1][1:]:
+        prev += i[-1]
+        prev2 = i[0] - prev2
+
+    tot += prev
+    tot2 += prev2
+
+print("Part 1:", tot)
+print("Part 2:", tot2)
